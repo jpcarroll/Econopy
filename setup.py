44338843dc4cb4896025b0d9e12e4a583627ca05
Jack Carroll <jpcarrrolll@gmail.com>
2017-06-09 20:54:10 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Econopy',
+      version='0.1',
+      description='Economic Inequality Metrics in Python',
+      author='Jack Carroll',
+      author_email='jpcarrrolll@gmail.com',
+      url='https://github.com/jpcarroll/Econopy',
+      download_url='https://github.com/mmngreco/IneqPy/tarball/0.0.2',
+      packages=['econopy'],
+      install_requires=['numpy', 'matplotlib'],
+     )
